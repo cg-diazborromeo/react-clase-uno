81e64aaf26338f04ba642376cfe2d663369fd00a
cg-diazborromeo <Gastadiaz@gmail.com>
2023-04-04 01:25:41 +0200

    feat: Comienza tarea 1
    realice el primer componente (Tarjeta)

--- src/index-mio.js
+++ src/index-mio.js
@@ -1,9 +1,20 @@
-import { Welcome } from "./components/Welcome.js";
+import { Tarjeta } from "./tarea/Tarea1.js";
 
 ReactDOM.render(
-  <Welcome nombre="Julián" />,
+  <Tarjeta
+    nombre="Carlos Gaston Diaz Borromeo"
+    titulo="Programador Front End"
+    imagen="https://scontent-cph2-1.xx.fbcdn.net/v/t39.30808-6/333553008_3455518144719385_2180972273582119643_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=fPqd9lPdOF4AX-NOmUV&_nc_ht=scontent-cph2-1.xx&oh=00_AfBnKdNDdyEk6Gms8WWEtBYPrRKQ99rs_RwyArFXuDL6yQ&oe=64300D28"
+  />,
   document.getElementById("react-app")
 );
+
+// import { Welcome } from "./components/Welcome.js";
+
+// ReactDOM.render(
+//   <Welcome nombre="Julián" />,
+//   document.getElementById("react-app")
+// );
 
 // import { Button } from './components/Button.js';
 
